928ce4d8ba6d2dd946f9963de2c558cdc303ef33
Daniel M. Lofaro <dan@danlofaro.com>
2012-02-23 16:09:31 -0500

    for rob

--- readCanEnc.m
+++ readCanEnc.m
@@ -1,19 +1,51 @@
-function [ t , n ] = readCanEnc( tFile )
+function [ mot ] = readCanEnc( tFile )
 
+addpath('huboJointConstants');			% add the hubo joint constants
+						% path
+huboJointConst;					% Run Hubo Joint Const
 
 baseVal			=	hex2dec('60');	% this is the base value for 
 						% ENC#_RXDF = 0x60 + #
 maxBaseVal		=	hex2dec('90');	% Maximum value (not including
 						% for enc read
 [ t, ch, id, dlc, d, L ] = readCan( tFile );	% get can data
-
+mot = [];
+moti = [];
+moti(jointMax) 		=	0;
 for(i = 1:L)
-	m 		=	a{i};	% get the current string to read
-	[s1, s2]	= 	strread(m,'%s%s','delimiter','Rx');
-	strTop 		= 	s1{1};	% part of the string with time
-	strBot		=	s1{2};	% part of the string with the data
-	strTopNa	=	strread(strTop,'%s','delimiter',' ');
-	t(i)		=	str2num(strTopNa{1});
-	bn 		= 	hex2dec(strTopNa{3}) - baseVal;
-	n(i)		=	hex2dec(strTopNa{3});
+	
+	if(id(i) >= baseVal & id(i) < maxBaseVal)
+
+		bn	=	id(i) - baseVal;	% current board num
+		bni	= 	bn + 1;			% board number index 
+		
+		mVal	=	jmcN(bni);
+		enc1	=	[];
+		if( mVal == 2 & dlc(i) == 8) 		% Two motor motor controller
+			%enc(1) = hex2dec([dec2hex(d(i,1)),dec2hex(d(i,2)),dec2hex(d(i,3)),dec2hex(d(i,4))]);
+			%enc(1) = hex2dec([dec2hex(d(i,4)),dec2hex(d(i,3)),dec2hex(d(i,2)),dec2hex(d(i,1))]);
+			%enc(2) = hex2dec([dec2hex(d(i,8)),dec2hex(d(i,7)),dec2hex(d(i,6)),dec2hex(d(i,5))]);
+			encN	=	[d{i}(4),d{i}(3),d{i}(2),d{i}(1)];
+			enc(1) = hex2dec(encN);
+			enc(2) = hex2dec([d{i}(8),d{i}(7),d{i}(6),d{i}(5)]);
+			%enc(2) = hex2dec([dec2hex(d(i,5)),dec2hex(d(i,6)),dec2hex(d(i,7)),dec2hex(d(i,8))]);
+			moti(bni) = moti(bni) + 1;
+			ii = moti(bni);
+
+			%% Motor 1
+			mot1 = jmc(bni,1);
+			mot(mot1,1,ii) = t(i);		% record the time
+			mot(mot1,2,ii) = typecast(uint32(enc(1)),'int32');		% record the pos
+			
+			%% Motor 2
+			mot2 = jmc(bni,2);	
+			mot(mot2,1,ii) = t(i);		% record the time
+			mot(mot2,2,ii) = typecast(uint32(enc(2)),'int32');		% record the pos
+
+			
+		end
+			
+
+	end
+
 end
